298d0433148de86eb393b3f2a046b4b0b6059b8e
LavaWaffle <60158215+LavaWaffle@users.noreply.github.com>
2024-03-07 23:51:52 -0500

    added animations + continuing discover

--- src/app/(tabs)/discover.tsx
+++ src/app/(tabs)/discover.tsx
@@ -1,15 +1,33 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
-  StyleSheet,
   TextInput,
   View,
   Keyboard,
-  Button,
   Text,
+  Dimensions,
+  Pressable,
 } from "react-native";
 import { Image } from "expo-image";
 import { Feather, Entypo } from "@expo/vector-icons";
+import Animated, {
+  ReduceMotion,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { FlashList } from "@shopify/flash-list";
+
+// Animated.addWhitelistedUIProps({ className: true, style: true });
+
+const animatedSettings = {
+  duration: 750,
+  dampingRatio: 1,
+  stiffness: 100,
+  overshootClamping: false,
+  restDisplacementThreshold: 0.01,
+  restSpeedThreshold: 2,
+  reduceMotion: ReduceMotion.System,
+};
 
 function SearchBar(props: {
   searchPhrase: string;
@@ -19,23 +37,56 @@ function SearchBar(props: {
 }) {
   const { searchPhrase, setSearchPhrase, clicked, setClicked } = props;
 
+  // get the width of the screen
+  const screenWidth = Dimensions.get("window").width;
+  const bigWidth = 0.9 * (screenWidth - 30);
+  const smallWidth = 0.75 * (screenWidth - 30);
+  const SearchBarWidth = useSharedValue(bigWidth);
+  const cancelWidth = useSharedValue(0);
+  const featherWidth = useSharedValue(30);
+  const entypoWidth = useSharedValue(0);
+
+  useEffect(() => {
+    SearchBarWidth.value = withSpring(
+      clicked ? smallWidth : bigWidth,
+      animatedSettings
+    );
+    cancelWidth.value = withSpring(clicked ? 80 : 0, animatedSettings);
+    featherWidth.value = withSpring(clicked ? 0 : 30, {
+      stiffness: 1,
+      duration: 2000,
+      ...animatedSettings,
+    });
+    entypoWidth.value = withSpring(clicked ? 30 : 0, animatedSettings);
+  }, [clicked]);
+
   return (
     <View className="m-[15px] flex flex-row items-center justify-center">
-      <View
+      <Animated.View
+        style={{
+          width: SearchBarWidth,
+        }}
         className={
           clicked
-            ? "p-[10px] flex flex-row w-[75%] bg-[#323436] rounded-[10%] items-center justify-evenly"
-            : "p-[10px] flex flex-row w-[95%] bg-[#323436] rounded-[10%] items-center"
+            ? "p-[10px] flex flex-row bg-[#323436] rounded-[50%] items-center justify-evenly"
+            : "p-[10px] flex flex-row bg-[#323436] rounded-[50%] items-center"
         }
       >
-        {!clicked && <Feather
-          name="search"
-          size={25}
-          color="#727477"
-          style={{ paddingLeft: 4, marginRight: 4 }}
-        />}
+        <Animated.View
+          style={{
+            width: featherWidth,
+            overflow: "hidden",
+          }}
+        >
+          <Feather
+            name="search"
+            size={25}
+            color="#727477"
+            style={{ paddingLeft: 4, marginRight: 4 }}
+          />
+        </Animated.View>
         <TextInput
-          className="text-[20px] ml-[10px] w-[90%] text-[#ECEBED]"
+          className="text-[15px] ml-[10px] w-[90%] text-[#ECEBED]"
           placeholder="Search"
           placeholderTextColor="#A2A2A2"
           value={searchPhrase}
@@ -46,7 +97,13 @@ function SearchBar(props: {
           }}
           onFocus={() => setClicked(true)}
         />
-        {clicked && (
+
+        <Animated.View
+          style={{
+            width: entypoWidth,
+            overflow: "hidden",
+          }}
+        >
           <Entypo
             name="cross"
             size={25}
@@ -58,27 +115,59 @@ function SearchBar(props: {
               setSearchPhrase("");
             }}
           />
-        )}
-      </View>
-      {clicked && (
-        <View>
-          <Button
-            title="cancel"
-            onPress={() => {
-              Keyboard.dismiss();
-              setClicked(false);
+        </Animated.View>
+      </Animated.View>
+
+      <Animated.View
+        style={{
+          width: cancelWidth,
+          overflow: "hidden",
+        }}
+      >
+        <Pressable
+          className="overflow-hidden h-[18px]"
+          onPress={() => {
+            Keyboard.dismiss();
+            setClicked(false);
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "Roboto_700Bold",
             }}
-          ></Button>
-        </View>
-      )}
+            className="text-blue-500 text-[17px] ml-2"
+          >
+            Cancel
+          </Text>
+        </Pressable>
+      </Animated.View>
     </View>
   );
 }
 
+function Chip(props: { text: string; clicked: boolean; onClick: any }) {
+  const { text, clicked, onClick } = props;
+  return (
+    <View className="rounded-lg ">
+      <Text
+        style={{ fontFamily: "Poppins_600SemiBold" }}
+        className={`p-2 rounded-lg text-[#ECEBED] text-[12px] border-2 ${
+          clicked
+            ? "bg-[#2E8AF6] border-[#2E8AF6]"
+            : "border-[#727477] bg-transparent"
+        } `}
+      >
+        {text}
+      </Text>
+    </View>
+  );
+}
+
+const chipData = ["All", "Robotics", "FBLA", "Sports", "HOSA", "DECA"];
+
 const discover = () => {
   const [searchPhrase, setSearchPhrase] = useState("");
   const [clicked, setClicked] = useState(false);
-  const [fakeData, setFakeData] = useState();
 
   return (
     <SafeAreaView>
@@ -88,6 +177,25 @@ const discover = () => {
         clicked={clicked}
         setClicked={setClicked}
       />
+
+      {/* Chips */}
+      <View className="space-x-2">
+        <FlashList
+          data={chipData}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          estimatedItemSize={chipData.length}
+          renderItem={({ item }) => (
+            <Chip
+              text={item}
+              clicked={false}
+              onClick={() => {
+                console.log(item);
+              }}
+            />
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 };
